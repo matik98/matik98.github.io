2487f1afc0e98bec10e039864528986cc969d01f
Mateusz Skąpski <hartez6829213@gmail.com>
2019-11-12 01:31:37 +0100

    Updated to list2

---
+++ Lista1/js/dropdown_menu.js
@@ -0,0 +1,12 @@
+let dropdownButton = document.getElementById("dropdownButton");
+dropdownButton.onclick = myFunction;
+
+function myFunction() {
+    let x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
