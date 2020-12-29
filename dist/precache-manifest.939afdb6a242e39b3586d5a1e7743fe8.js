4a7c4a57e2bd552d99cf1f35293c45555cdc14fe
Mateusz Skąpski <hartez6829213@gmail.com>
2020-12-30 00:30:45 +0100

    Add files via upload

---
+++ dist/precache-manifest.939afdb6a242e39b3586d5a1e7743fe8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6f89a1fbb2c4d400673",
+    "url": "/css/app.5ecc2c3a.css"
+  },
+  {
+    "revision": "4a91291df6eaabed70db",
+    "url": "/css/chunk-vendors.ddfdcb8f.css"
+  },
+  {
+    "revision": "69c110242dbb0f28a96d0cf20818325c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6f89a1fbb2c4d400673",
+    "url": "/js/app.753894ac.js"
+  },
+  {
+    "revision": "4a91291df6eaabed70db",
+    "url": "/js/chunk-vendors.f002b6cd.js"
+  },
+  {
+    "revision": "5d913334cbba504cb826df6a21535ddb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
